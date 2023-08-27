117a2b3cfa7e4af85b8da14d635667be3bd9ad31
Barasa-Micah <barasamica@gmail.com>
2023-08-27 09:36:46 +0300

    Completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
-class MyString:
-  pass
+class MyString():
+    def __init__(self,value):
+        if not isinstance(value,str):
+            raise ValueError('The vaue must be a string.')
+        
+        self._value = value
+
+
+        @property
+        def value(self):
+            return self._value
+        
+        @value.setter
+        def value(self, new_value):
+            if not isinstance(new_value,str):
+                raise ValueError('The value must be a string.')
+            
+            self._value = new_value
